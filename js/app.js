ebe0cc14dcd94d83e295005f689a37de3193aaa7
Alejandro Carranza <crrzb@hotmail.com>
2022-10-27 22:07:03 -0500

    agregue ojos

--- js/app.js
+++ js/app.js
@@ -2,26 +2,56 @@ function setup() {
 
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext("2d");
-    //PEGAR AQUI SUS CAMBIOS
-ctx.strokeRect(0, 0, 300, 300);
-ctx.strokeStyle = 'rgb(0, 0, 0)';
-ctx.strokeRect(60, 50, 50, 80);
-ctx.strokeRect(190, 50, 50, 80);
-ctx.strokeRect(130, 150, 100, 20);
-ctx.strokeRect(100, 200, 100, 1);
 
-ctx.arc(90,90,20,0,(Math.PI/180)*360,true);
-ctx.fillStyle="#3cbbdf";
-ctx.fill();
-ctx.arc(220,90,20,0,(Math.PI/180)*360,true);
-ctx.fillStyle="#3cbbdf";
-ctx.fill();
-ctx.beginPath();
-ctx.arc(90,90,10,0,(Math.PI/180)*360,true);
-ctx.fillStyle="#000000";
-ctx.fill();
-ctx.arc(220,90,10,0,(Math.PI/180)*360,true);
-ctx.fillStyle="#000000";
-ctx.fill();
-ctx.finishpath();
+    canvas.height = 600;
+    canvas.width = 600;
+    //PEGAR AQUI SUS CAMBIOS
+
+    //CUERPO
+
+    ctx.beginPath();
+    ctx.strokeRect(50, 50, 300, 300);
+    ctx.fillStyle = "#fff56c";
+    ctx.fill();
+
+    //OJOS 
+
+    //OJO IZQ
+    ctx.arc(140, 120, 50, 0, (Math.PI) * 2, true);
+    ctx.stroke();
+
+    //OJO DER
+    ctx.beginPath();
+    ctx.arc(260, 120, 50, 0, (Math.PI) * 2, true);
+    ctx.stroke();
+
+
+    ctx.strokeRect(190, 180, 150, 20);
+
+    ctx.beginPath();
+    ctx.arc(200, 200, 80, 0, (Math.PI));
+    ctx.stroke();
+
+
+
+    ctx.beginPath();
+    ctx.arc(140, 120, 20, 0, (Math.PI / 180) * 360, true);
+    ctx.fillStyle = "#3cbbdf";
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(140, 120, 10, 0, (Math.PI / 180) * 360, true);
+    ctx.fillStyle = "#000000";
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(260, 120, 20, 0, (Math.PI / 180) * 360, true);
+    ctx.fillStyle = "#3cbbdf";
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(260, 120, 10, 0, (Math.PI / 180) * 360, true);
+    ctx.fillStyle = "#000000";
+    ctx.fill();
+
 }
