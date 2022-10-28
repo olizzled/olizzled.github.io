31896b0112ef0a14fc7a018a44586b6fc8b625cf
Alejandro Carranza <crrzb@hotmail.com>
2022-10-27 21:08:24 -0500

    diseño act 6, ligada a index

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function setup() {
+
+    const canvas = document.querySelector("#canvas");
+    const ctx = canvas.getContext("2d");
+
+    //Resize onload
+    canvas.height = 600;
+    canvas.width = 600;
+
+
+    //BOB ESPONJA
+
+    // ----------------------------------------------------------------------------
+    // ---------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
+    //PEGAR AQUI SUS CAMBIOS
+
+
+
+}
